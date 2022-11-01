fff6b3e38c8d6ca06df8d180b3bc4e5e6435a865
LuisFerTR <luisfertalavera15@gmail.com>
2022-11-01 11:58:37 -0600

    Remover comentarios de pruebas

--- scripts/dibujar_tabla.php
+++ scripts/dibujar_tabla.php
@@ -43,29 +43,4 @@ function dibujar_tabla(string $tabla)
     }
 
     echo '<table>';
-    /*
-
-    echo '<table>';
-    // Display table header
-    echo '<thead>';
-    echo '<tr>';
-    foreach ($res->fetch_fields() as $column) {
-        echo '<th>' . htmlspecialchars($column->name) . '</th>';
-    }
-    echo '</tr>';
-    echo '</thead>';
-    // If there is data then display each row
-    if ($data) {
-        foreach ($data as $row) {
-            echo '<tr>';
-            foreach ($row as $cell) {
-                echo '<td>' . htmlspecialchars($cell) . '</td>';
-            }
-            echo '</tr>';
-        }
-    } else {
-        echo '<tr><td colspan="' . $res->field_count . '">No records in the table!</td></tr>';
-    }
-    echo '</table>';
-    */
 }
